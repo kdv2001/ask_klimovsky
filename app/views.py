5e6c23ee3ad067b5857e78deb3cd0b91856cbed2
kdv <kdv200330@bk.ru>
2021-11-08 01:19:00 +0300

    do template

--- app/views.py
+++ app/views.py
@@ -2,31 +2,88 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 from django.http import HttpResponse
 
-
 # Create your views here
-
-
-def index(request):
-    return render(request, "index.html", {})
-
-
-questions = [
+questions_hot = [
     {
-        "title": f"Title {i}",
-        "text": f"This is text for {i} question."
+        "title": f"Title_hot {i}",
+        "text": f"This is for {i} question_base.",
+        "number": i
+    } for i in range(100)
+]
+
+questions_base = [
+    {
+        "title": f"Title_base {i}",
+        "text": f"This is text for {i} question_base.",
+        "number": i
+    } for i in range(100)
+]
+
+questions_tag = [
+    {
+        "title": f"Title_tag {i}",
+        "text": f"This is text for {i} question_tag.",
+        "number": i
+    } for i in range(100)
+]
+
+question_num = [
+    {
+        "title": f"Title_question",
+        "text": f"This is text for  question_question.",
+        "number": 1
+    }
+]
+
+answers = [
+    {
+        "title": f"Title_tag {i} answer_title",
+        "text": f"This is text for {i} answer_text.",
+        "number": i
     } for i in range(100)
 ]
 
 
+
+def index(request):
+    paginator = Paginator(questions_base, 5)
+    page = request.GET.get('page')
+    content_base = paginator.get_page(page)
+    return render(request, "index.html", {'questions': content_base})
+
+
 def hot(request):
-    paginator = Paginator(questions, 5)
+    paginator = Paginator(questions_hot, 5)
     page = request.GET.get('page')
     content = paginator.get_page(page)
     return render(request, "hot.html", {'questions': content})
 
 
-def question(request):
-    return render(request, "question.html", {})
+def tag(request):
+    paginator = Paginator(questions_tag, 5)
+    page = request.GET.get('page')
+    content_tag = paginator.get_page(page)
+    return render(request, "base.html", {'questions': content_tag})
 
-# def index(request):
-#     return render(request, "base.html", {})
+
+def question(request, number):
+    paginator = Paginator(answers, 5)
+    page = request.GET.get('page')
+    content_answer = paginator.get_page(page)
+    return render(request, "question.html", {'questions': questions_base[number], 'answers': content_answer})
+
+
+def ask(request):
+    return render(request, "ask.html", {})
+
+
+def login(request):
+    return render(request, "login.html", {})
+
+
+def signup(request):
+    return render(request, "signup.html", {})
+
+
+def settings(request):
+    return render(request, "settings.html", {})
--- askme/urls.py
+++ askme/urls.py
@@ -20,13 +20,13 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hot/', views.hot),
-    # path('tag/', views.tag),
-    path('question/', views.question),
-    # path('login/', views.login),
-    # path('signup/', views.signup),
-    # path('ask/', views.ask),
-    # path('new/', views.index),
-    path('', views.index)
+    path('hot/', views.hot, name="hot"),
+    path('tag/', views.tag, name="tag"),
+    path('question/<int:number>/', views.question, name="question"),
+    path('login/', views.login, name="login"),
+    path('signup/', views.signup, name="signup"),
+    path('ask/', views.ask, name="ask"),
+    path('settings/', views.settings, name="settings"),
+    path('', views.index, name="index")
 
 ]
